5796403785c801019f715e030d753b9afa46c87b
Alex <jience520@163.com>
2018-06-04 21:48:25 +0800

    2.3a 内置登录方法

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    path('login/', views.user_login, name='user_login')
+    # path('login/', views.user_login, name='user_login'),
+    path('login/', auth_views.login, name='user_login'),
+    path('new-login/', auth_views.login, {"template_name": "account/login.html"}),
 ]
 
 app_name = 'account'
