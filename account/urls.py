3b5e982c7c966ba548a4769ee205033ce4cfc881
Alex <jience520@163.com>
2018-06-04 22:01:54 +0800

    2.3c 内置退出方法

--- account/urls.py
+++ account/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     # path('login/', views.user_login, name='user_login'),
     path('login/', auth_views.login, name='user_login'),
     path('new-login/', auth_views.login, {"template_name": "account/login.html"}),
+    path('logout/', auth_views.logout, {"template_name": "account/logout.html"}, name='user_logout'),
 ]
 
 app_name = 'account'
