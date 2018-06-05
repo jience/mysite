5e09463b0bc2ea1f828c9706bd8484d0bffe8fb7
Alex <jience520@163.com>
2018-06-05 22:32:16 +0800

    2.5a 修改密码

--- account/urls.py
+++ account/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('new-login/', auth_views.login, {"template_name": "account/login.html"}),
     path('logout/', auth_views.logout, {"template_name": "account/logout.html"}, name='user_logout'),
     path('register/', views.register, name='user_register'),
+    path('password-change/', auth_views.password_change, {"post_change_redirect": "/account/password-change-done"}, name='password_change'),
+    path('password-change-done', auth_views.password_change_done, name='password_change_done'),
 ]
 
 app_name = 'account'
