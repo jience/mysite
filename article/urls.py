0f50a111d0e6a297ba83753d73914929bcdc2f78
Alex <jience520@163.com>
2018-07-01 16:26:11 +0800

    3.2c 文章标题列表

--- article/urls.py
+++ article/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^rename-column/$', views.rename_article_column, name="rename_article_column"),
     url(r'^del-column/$', views.del_article_column, name="del_article_column"),
     url(r'^article-post/$', views.article_post, name="article_post"),
+    url(r'^article-list/$', views.article_list, name="article_list"),
+    url(r'^article-detail/(?P<id>\d+)/(?P<slug>[-\w]+)$', views.article_detail, name="article_detail"),
 ]
--- article/views.py
+++ article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
@@ -75,4 +75,16 @@ def article_post(request):
         article_post_form = ArticlePostForm()
         article_columns = request.user.article_column.all()
         return render(request, "article/column/article_post.html", {"article_post_form": article_post_form,
-                                                             "article_columns": article_columns})
+                                                                    "article_columns": article_columns})
+
+
+@login_required(login_url='/account/login/')
+def article_list(request):
+    articles = ArticlePost.objects.filter(author=request.user)
+    return render(request, "article/column/article_list.html", {"articles": articles})
+
+
+@login_required(login_url='/account/login/')
+def article_detail(request, id, slug):
+    article = get_object_or_404(ArticlePost, id=id, slug=slug)
+    return render(request, "article/column/article_detail.html", {"article": article})
