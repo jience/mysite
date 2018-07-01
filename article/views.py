359723c5717e2b7dcc391f2346a24b27faff041a
Alex <jience520@163.com>
2018-07-01 21:44:12 +0800

    3.2d 删除文章

--- article/views.py
+++ article/views.py
@@ -88,3 +88,16 @@ def article_list(request):
 def article_detail(request, id, slug):
     article = get_object_or_404(ArticlePost, id=id, slug=slug)
     return render(request, "article/column/article_detail.html", {"article": article})
+
+
+@login_required(login_url='/account/login')
+@require_POST
+@csrf_exempt
+def del_article(request):
+    article_id = request.POST['article_id']
+    try:
+        article = ArticlePost.objects.get(id=article_id)
+        article.delete()
+        return HttpResponse("1")
+    except:
+        return HttpResponse("2")
