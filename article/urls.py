d64a267ad4eb72f7389ebf7e1a7d051f71af135e
Alex <jience520@163.com>
2018-07-08 21:16:59 +0800

    4.2 查看作者全部文章

--- article/urls.py
+++ article/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     url(r'^del-article$', views.del_article, name="del_article"),
     url(r'^redit-article/(?P<article_id>\d+)/$', views.redit_article, name="redit_article"),
     url(r'^list-article-titles/$', list_views.article_titles, name="article_titles"),
+    url(r'^list-article-titles/(?P<username>[-\w]+)/$', list_views.article_titles, name="author_articles"),
     url(r'^list-article-detail/(?P<id>\d+)/(?P<slug>[-\w]+)$', list_views.article_detail, name="list_article_detail"),
 ]
