350d0de105f746681bab806243454e8934c8a687
Alex <jience520@163.com>
2018-07-19 22:58:05 +0800

    4.7c 推荐相似文章

--- article/list_views.py
+++ article/list_views.py
@@ -9,6 +9,7 @@ from django.views.decorators.http import require_POST
 from django.http import HttpResponse
 import redis
 from django.conf import settings
+from django.db.models import Count
 
 r = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
 
@@ -61,10 +62,14 @@ def article_detail(request, id, slug):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    article_tags_ids = article.article_tag.values_list("id", flat=True)
+    similar_articles = ArticlePost.objects.filter(article_tag__in=article_tags_ids).exclude(id=article.id)
+    similar_articles = similar_articles.annotate(same_tags=Count("article_tag")).order_by('-same_tags', '-created')[:4]
     return render(request, "article/list/article_detail.html", {"article": article,
                                                                 "total_views": total_views,
                                                                 "most_viewed": most_viewed,
-                                                                "comment_form": comment_form})
+                                                                "comment_form": comment_form,
+                                                                "similar_articles": similar_articles})
 
 
 @csrf_exempt
