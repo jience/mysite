1f6cb2ec30c735b8f10dd8365b27396752212dc2
Alex <jience520@163.com>
2018-07-22 13:10:18 +0800

    5.1a 图片上传和管理功能

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'blog',
     'account',
     'article',
+    'image',
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
     url(r'^account/', include('account.urls', namespace='account', app_name='account')),
     url(r'^article/', include('article.urls', namespace='article', app_name='article')),
+    url(r'^image/', include('image.urls', namespace='image', app_name='image')),
 ]
