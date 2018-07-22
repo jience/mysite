3a47b12a7c34b5434f5dbb228e9713f7951191cb
Alex <jience520@163.com>
2018-07-22 17:54:30 +0800

    7.1b 读取数据

--- course/urls.py
+++ course/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
+from .views import CourseListView
 
 
 urlpatterns = [
     url(r'^about/$', TemplateView.as_view(template_name="course/about.html"), name="about"),
+    url(r'^course-list/$', CourseListView.as_view(), name="course_list"),
 ]
--- course/views.py
+++ course/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from django.contrib.auth.models import User
+from .models import Course
 
-# Create your views here.
+
+class CourseListView(ListView):
+    model = Course
+    context_object_name = "course"
+    template_name = 'course/course_list.html'
+
+    def get_queryset(self):
+        qs = super(CourseListView, self).get_queryset()
+        return qs.filter(user=User.objects.filter(username='alex'))
