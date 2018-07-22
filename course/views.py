9469f8c15e3091d9527aa15263c1b1d90b4a7fe2
Alex <jience520@163.com>
2018-07-22 18:36:38 +0800

    7.2a 创建课程

--- course/views.py
+++ course/views.py
@@ -1,6 +1,15 @@
 from django.views.generic import TemplateView, ListView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.models import User
+from django.shortcuts import redirect
 from .models import Course
+from .forms import CreateCourseForm
+
+from braces.views import LoginRequiredMixin
+
+
+class AboutView(TemplateView):
+    template_name = "course/about.html"
 
 
 class CourseListView(ListView):
@@ -19,10 +28,25 @@ class UserMixin:
         return qs.filter(user=self.request.user)
 
 
-class UserCourseMixin(UserMixin):
+class UserCourseMixin(UserMixin, LoginRequiredMixin):
     model = Course
+    login_url = "/account/login/"
 
 
 class ManageCourseListView(UserCourseMixin, ListView):
     context_object_name = "courses"
     template_name = 'course/manage/manage_course_list.html'
+
+
+class CreateCourseView(UserCourseMixin, CreateView):
+    fields = ['title', 'overview']
+    template_name = 'course/manage/create_course.html'
+
+    def post(self, request, *args, **kargs):
+        form = CreateCourseForm(data=request.POST)
+        if form.is_valid():
+            new_course = form.save(commit=False)
+            new_course.user = self.request.user
+            new_course.save()
+            return redirect("course:manage_course")
+        return self.render_to_response({"form": form})
